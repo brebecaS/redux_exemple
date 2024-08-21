42b746769f5acdb60f4906dc1373eadbaea73778
brebecaS <birleanu0rebeca@gmail.com>
2024-08-21 19:18:24 +0300

    added redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { decrementByOne } from "./counterSlice";
 
 function App() {
+  // Selection sate from store
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        aria-label="Increment by one"
+        onClick={() => {
+          dispatch(decrementByOne());
+        }}
+      >
+        Increment
+      </button>
+      <button aria-label="Decrement by one">Decrement</button>
+      <button aria-label="Increment by two"> Increment by 2</button>
+      <h2>{counter}</h2>
     </div>
   );
 }
---
+++ src/counterSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// 1. Create slice
+export const counterSlice = createSlice({
+  // 1. Name of the slice
+  name: "counter",
+  // 2. Initial state
+  initialState: {
+    counter: 0,
+  },
+  // 3. Reducers
+  reducers: {
+    incrementByValue: (state, action) => {
+      state.counter = state.counter + action.payload;
+    },
+    incrementByOne: (state) => {
+      state.counter = state.counter - 1;
+    },
+    decrementByOne: (state) => {
+      state.counter = state.counter + 1;
+    },
+  },
+});
+
+// 2. Export actions
+export const { incrementByValue, incrementByOne, decrementByOne } =
+  counterSlice.actions;
+
+// 3. Export reducer
+export default counterSlice.reducer;
